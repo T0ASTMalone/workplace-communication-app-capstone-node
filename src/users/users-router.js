36db16c6b78f70541c862241db634dcbc440a35d
Miguel Ponce <miguelaponce258@gmail.com>
2019-12-13 17:06:22 -0700

    fixed user type on post

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const usersService = require("./users-service");
+const requireAuth = require("../auth/auth-router");
 const usersRouter = express.Router();
 
 const jsonParser = express.json();
@@ -18,35 +19,38 @@ usersRouter
   })
   .post(jsonParser, (req, res, next) => {
     const knex = req.app.get("db");
-    const { username, password, code, type, nickname, img } = req.body;
+    const { username, password, code, nickname, img } = req.body;
+    // find WorkPlace with code provided
+    // create user object that includes the wpId and wpName
+    const user = {
+      username,
+      password,
+      nickname,
+      img
+    };
+    // check if there is a user in the req body
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: { message: `Missing user in request body` } });
+    }
 
     usersService
       .getWp(knex, code)
       .then(wp => {
+        // if WorkPlace with provided code does not exists
+        // return wp not found
         if (!wp) {
           return res
             .status(404)
             .json({ error: { message: `Invalid WorkPlace Code` } });
         }
+        // else get wpId and wpName if WorkPlace
+        user.type = "pending";
+        user.wp_id = wp.wp_id;
+        user.wp_name = wp.wp_name;
 
-        let wp_id = wp.wp_id;
-        let wp_name = wp.wp_name;
-        const user = {
-          username,
-          password,
-          wp_id,
-          wp_name,
-          type,
-          nickname,
-          img
-        };
-
-        if (!user) {
-          return res
-            .status(400)
-            .json({ error: { message: `Missing user in request body` } });
-        }
-
+        // check for missing fields
         for (const [key, value] of Object.entries(user)) {
           if (value == null) {
             if (key !== "nickname" && key !== "img") {
@@ -56,21 +60,27 @@ usersRouter
             }
           }
         }
-
+        // validate password
         const passErr = usersService.validatePassword(password);
         if (passErr) {
           return res.status(400).json({ error: { message: passErr } });
         }
-
-        usersService.usrExists(knex, username, wp_id).then(existing => {
+        // validate that users with that username does not already exists
+        // in the WorkPlace
+        usersService.usrExists(knex, username, user.wp_id).then(existing => {
+          // if username already exists in WorkPlace
+          // return user already exists
           if (existing) {
             return res
               .status(400)
               .json({ error: { message: "User already exists" } });
           }
+          // hash the users password
           return usersService.hashPass(password).then(hashedPass => {
             user.password = hashedPass;
+            // create user
             return usersService.createUser(knex, user).then(user => {
+              // respond with new user
               res
                 .status(201)
                 .location(path.posix.join(req.originalUrl + `/${user.user_id}`))
@@ -130,22 +140,29 @@ usersRouter
       .catch(next);
   });
 
-usersRouter.route("/wp/:wpId").all((req, res, next) => {
-  const knex = req.app.get("db");
-  const id = req.params.wpId;
-  usersService
-    .getWpUsers(knex, id)
-    .then(users => {
-      if (!users) {
+// wp specific users router
+// use requireAuth
+usersRouter
+  .route("/wp/:wpId")
+  .all(requireAuth)
+  .get((req, res, next) => {
+    const knex = req.app.get("db");
+    const id = req.params.wpId;
+    usersService
+      .getWpUsers(knex, id)
+      .then(users => {
+        if (!users) {
+          return res.status(404).json({
+            error: { message: `There are no users in this WorkPlace` }
+          });
+        }
         return res
-          .status(404)
-          .json({ error: { message: `There are no users in this WorkPlace` } });
-      }
-      return res
-        .status(200)
-        .json(users.map(user => usersService.serializeUser(user)));
-    })
-    .catch(next);
-});
+          .status(200)
+          .json(users.map(user => usersService.serializeUser(user)));
+      })
+      .catch(next);
+  });
+
+// wp specific users router for getting pending users
 
 module.exports = usersRouter;
