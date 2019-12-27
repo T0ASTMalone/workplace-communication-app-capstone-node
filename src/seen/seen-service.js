44a49e099dda765c38c2a6fa85e5cac706e2395b
Miguel Ponce <miguelaponce258@gmail.com>
2019-12-26 19:42:07 -0700

    adding services for acknowledgements route

---
+++ src/seen/seen-service.js
@@ -0,0 +1,47 @@
+const xss = require("xss");
+
+const postsService = {
+  getAllAcks(db) {
+    return db("seen")
+      .innerJoin("users", "seen.user_id", "users.user_id")
+      .select("seen.*", "users.nickname");
+  },
+
+  createPost(db, ack) {
+    return db("seen")
+      .insert(ack)
+      .returning("*")
+      .then(rows => rows[0]);
+  },
+
+  getAckById(db, id) {
+    return db("posts")
+      .innerJoin("users", "seen.user_id", "users.user_id")
+      .select("seen.*", "users.nickname")
+      .where({ "seen.id": id })
+      .first();
+  },
+
+  deleteAck(db, id) {
+    return db("seen")
+      .where({ id })
+      .delete();
+  },
+
+  getPostAcks(db, post_id) {
+    return db("seen")
+      .innerJoin("users", "seen.user_id", "users.user_id")
+      .select("seen.*", "users.nickname")
+      .where({ "seen.post_id": post_id });
+  },
+
+  serializePost(ack) {
+    return {
+      user_id: ack.user_id,
+      nickname: xss(ack.nickname),
+      post_id: ack.post_id
+    };
+  }
+};
+
+module.exports = postsService;
