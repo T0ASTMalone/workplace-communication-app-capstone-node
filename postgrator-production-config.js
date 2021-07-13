719ea882b477fdf957fa1d095270bc1ce439f5c3
T0ASTMalone <miguelaponce258@gmail.com>
2021-07-12 20:30:18 -0700

    reverted to old config

--- postgrator-production-config.js
+++ postgrator-production-config.js
@@ -7,8 +7,8 @@ module.exports = {
     process.env.NODE_ENV === "development"
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
-  // ssl: !!process.env.SSL,
-  ssl: true,
+  ssl: !!process.env.SSL,
+  // ssl: true,
   host: process.env.PROD_MIGRATION_DB_HOST,
   port: process.env.PROD_MIGRATION_DB_PORT,
   database: process.env.PROD_MIGRATION_DB_NAME,
