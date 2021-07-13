8ca353172d60837981701e3d858ea9a0d00a8e13
T0ASTMalone <miguelaponce258@gmail.com>
2021-07-12 20:02:52 -0700

    updating logging

--- src/app.js
+++ src/app.js
@@ -30,12 +30,13 @@ app.get("/", (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
+  response = { message: error.message, error };
+  // if (NODE_ENV === "production") {
+  //   response = { error: { message: "server error" } };
+  // } else {
+  //   console.error(error);
+  //   response = { message: error.message, error };
+  // }
   res.status(500).json(response);
 });
 
